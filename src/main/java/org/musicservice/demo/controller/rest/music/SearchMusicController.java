02391ef294571c0737f5a88d6ac6a0bd1496ecd9
Igor <igor.bocharov.77@gmail.com>
2026-03-20 11:06:25 +0300

    feat: add endpoints for search controller

--- src/main/java/org/musicservice/demo/controller/rest/music/SearchMusicController.java
+++ src/main/java/org/musicservice/demo/controller/rest/music/SearchMusicController.java
@@ -1,6 +1,9 @@
 package org.musicservice.demo.controller.rest.music;
 
+import org.musicservice.demo.dto.music.album.AlbumsResponse;
+import org.musicservice.demo.dto.music.artist.ArtistsResponse;
 import org.musicservice.demo.dto.music.search.SearchMusicResponse;
+import org.musicservice.demo.dto.music.sound.TracksResponse;
 import org.musicservice.demo.service.search.SearchMusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +23,20 @@ public class SearchMusicController {
     @GetMapping("/{fragment}")
     public ResponseEntity<SearchMusicResponse> searchStartingWith(@PathVariable (value = "fragment", required = false) String fragment){
         return ResponseEntity.ok(searchMusicService.searchMusicResult(fragment));
+    }
+
+    @GetMapping("/{fragment}/artists")
+    public ResponseEntity<ArtistsResponse> foundArtistsView(@PathVariable (value = "fragment", required = false) String fragment){
+        return ResponseEntity.ok(searchMusicService.getAllFoundArtists(fragment));
+    }
+
+    @GetMapping("/{fragment}/albums")
+    public ResponseEntity<AlbumsResponse> foundAlbumsView(@PathVariable (value = "fragment", required = false) String fragment){
+        return ResponseEntity.ok(searchMusicService.getAllFoundAlbums(fragment));
+    }
+
+    @GetMapping("/{fragment}/tracks")
+    public ResponseEntity<TracksResponse> foundTracksView(@PathVariable (value = "fragment", required = false) String fragment){
+        return ResponseEntity.ok(searchMusicService.getAllFoundTracks(fragment));
     }
 }
