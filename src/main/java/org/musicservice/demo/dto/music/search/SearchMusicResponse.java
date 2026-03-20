4038bd9173de22bdc03cf46089e823cb672783f0
Igor <igor.bocharov.77@gmail.com>
2026-03-20 11:11:25 +0300

    feat: add new response tracks

--- src/main/java/org/musicservice/demo/dto/music/search/SearchMusicResponse.java
+++ src/main/java/org/musicservice/demo/dto/music/search/SearchMusicResponse.java
@@ -1,7 +1,9 @@
 package org.musicservice.demo.dto.music.search;
+
 import org.musicservice.demo.dto.music.album.AlbumResponse;
 import org.musicservice.demo.dto.music.artist.ArtistResponse;
+import org.musicservice.demo.dto.music.sound.SoundResponse;
 
 import java.util.List;
 
-public record SearchMusicResponse(List<ArtistResponse> artists, List<AlbumResponse> albums) {}
+public record SearchMusicResponse(List<SoundResponse> tracks, List<AlbumResponse> albums, List<ArtistResponse> artists) {}
