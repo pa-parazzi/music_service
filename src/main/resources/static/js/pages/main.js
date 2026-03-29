375ab01c7635904482a979ea4beff225f9bfc025
Igor <igor.bocharov.77@gmail.com>
2026-03-29 16:46:47 +0300

    replace init player

--- src/main/resources/static/js/pages/main.js
+++ src/main/resources/static/js/pages/main.js
@@ -1,7 +1,9 @@
 import {initSearchForm} from "../module/search.js";
 import {initSidebar} from "../module/sidebar.js";
+import {initPlayer} from "../module/player.js";
 
 async function initMainPage(){
+    initPlayer();
     const searchForm = document.getElementById("search-form");
     initSearchForm(searchForm);
 }
