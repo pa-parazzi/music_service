ff699dbff0255479e60bb8027532ed68b5fb65b8
Igor <igor.bocharov.77@gmail.com>
2026-03-29 21:25:40 +0300

    refactor: rename mapping

--- src/main/java/org/musicservice/demo/controller/rest/music/MusicCollectionController.java
+++ src/main/java/org/musicservice/demo/controller/rest/music/MusicCollectionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/collection")
+@RequestMapping("/api/collection")
 public class MusicCollectionController {
 
     private final SoundService soundService;
