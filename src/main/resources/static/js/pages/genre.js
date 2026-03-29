e086bb4fa55ddf452a2b6d0e34d0b3f3a8a266f1
Igor <igor.bocharov.77@gmail.com>
2026-03-29 16:44:04 +0300

    refactor genre page

--- src/main/resources/static/js/pages/genre.js
+++ src/main/resources/static/js/pages/genre.js
@@ -2,8 +2,10 @@ import {initSidebar} from "../module/sidebar.js";
 import {getGenres} from "../api/genreApi.js";
 import {renderGenres} from "../components/genresView.js";
 import {initSearchForm} from "../module/search.js";
+import {initPlayer} from "../module/player.js";
 
 async function initGenrePage(){
+    initPlayer();
     const searchForm = document.getElementById("search-form");
     initSearchForm(searchForm);
 
