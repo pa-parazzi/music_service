95beba66d3b11f0c0729961ed7b91d1ff100d84f
Igor <igor.bocharov.77@gmail.com>
2026-03-29 16:42:59 +0300

    refactor album page

--- src/main/resources/static/js/pages/album.js
+++ src/main/resources/static/js/pages/album.js
@@ -7,40 +7,41 @@ import {renderAlbum} from "../components/albumView.js";
 import {getAlbumLike} from "../api/albumLikeApi.js";
 import {initAlbumLikes} from "../module/albumLikes.js";
 import {renderSounds} from "../components/soundsView.js";
-import {initSoundLikes} from "../module/soundLikes.js";
 import {initSearchForm} from "../module/search.js";
 import {initPlayer} from "../module/player.js";
+import {initTracksDelegation} from "../module/tracks.js";
+import {initPlayAlbumButton} from "../module/albums.js";
+import {paginationState} from "../store/PaginationState.js";
+import {playerState} from "../store/playerState.js";
 
 async function initAlbumPage() {
+    initPlayer();
+    const jwt = getToken();
+
     const id = window.location.pathname.split('/').pop();
 
     const album = await getAlbumById(id);
     const soundList = await getSoundListByAlbumId(id);
+    paginationState.tracks = soundList;
 
     const searchForm = document.getElementById("search-form");
     initSearchForm(searchForm);
 
     const albumContainer = document.getElementById('album-container');
     renderAlbum(albumContainer, album);
+    const playAlbumBtn = document.querySelector(".play-album-btn");
+    playerState.currentPlayAlbumButton = playAlbumBtn;
+    initPlayAlbumButton(id, playAlbumBtn);
+
+    const albumLikeBtn = document.querySelector(".album-like-btn");
+    const statusLikedAlbum = await getAlbumLike(id, jwt);
+    await initAlbumLikes(id, statusLikedAlbum, albumLikeBtn, jwt);
 
     const trackListContainer = document.getElementById('tracklist');
-    renderSounds(trackListContainer, soundList);
-
-    const trackCards = document.querySelectorAll(".track-card");
-    const playAlbumBtn = document.querySelector(".play-album-btn");
-    const albumLikeBtn = document.querySelector(".album-like-btn");
-    const soundLikeButtons = document.querySelectorAll(".like-btn");
-
-    await initPlayer({tracks: soundList, playAlbumBtnFromSinglePage: playAlbumBtn, trackCards: trackCards});
-
-    const jwt = getToken();
-    const likedSounds = await getSoundLikes(jwt);
-
-    await initSoundLikes(likedSounds, soundLikeButtons, jwt);
-
-    const statusLikedAlbum = await getAlbumLike(id, jwt);
-
-    await initAlbumLikes(id, statusLikedAlbum, albumLikeBtn, jwt);
+    const soundLikes = await getSoundLikes(jwt);
+    const likedSoundsIds = new Set(soundLikes.ids);
+    renderSounds({container: trackListContainer, soundList: soundList, likedSoundsIds: likedSoundsIds});
+    initTracksDelegation(trackListContainer, likedSoundsIds, jwt, id);
 }
 
 document.addEventListener("componentsLoaded", async () => {
