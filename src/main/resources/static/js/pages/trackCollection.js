0c835e9fec34d8b3f4912a480733c390b946d4e5
Igor <igor.bocharov.77@gmail.com>
2026-03-29 16:47:51 +0300

    refactor tracks collection page

--- src/main/resources/static/js/pages/trackCollection.js
+++ src/main/resources/static/js/pages/trackCollection.js
@@ -3,29 +3,29 @@ import {initSidebar} from "../module/sidebar.js";
 import {getToken} from "../user/auth.js";
 import {getSoundCollection} from "../api/soundCollectionApi.js";
 import {renderSounds} from "../components/soundsView.js";
-import {initSoundLikes} from "../module/soundLikes.js";
 import {initSearchForm} from "../module/search.js";
 import {initPlayer} from "../module/player.js";
+import {initTracksDelegation} from "../module/tracks.js";
+import {paginationState} from "../store/PaginationState.js";
 
 export async function initTrackCollectionPage(){
+    initPlayer();
+    const jwt = getToken();
+
     const searchForm = document.getElementById("search-form");
     initSearchForm(searchForm);
-
-    const jwt = getToken();
 
     const trackCollection = document.getElementById("track-collection");
 
     const likedSounds = await getSoundLikes(jwt);
+    const likedSoundIds = new Set(likedSounds.ids);
 
     const collectionData = await getSoundCollection(likedSounds);
     const soundList = collectionData.soundList;
 
-    renderSounds(trackCollection, soundList);
-    const soundLikeButtons = document.querySelectorAll('.like-btn');
-    await initSoundLikes(likedSounds, soundLikeButtons, jwt);
-
-    const trackCards = document.querySelectorAll('.track-card');
-    await initPlayer({tracks: soundList, trackCards: trackCards});
+    renderSounds({container: trackCollection, soundList: soundList, likedSoundsIds: likedSoundIds});
+    paginationState.tracks = soundList;
+    initTracksDelegation(trackCollection, likedSoundIds, jwt);
 }
 document.addEventListener("componentsLoaded", async () => {
     initSidebar();
