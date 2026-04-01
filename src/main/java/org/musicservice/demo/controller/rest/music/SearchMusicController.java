4d8fbde8b1d347b9a482903b4bbab046200b0209
Igor <igor.bocharov.77@gmail.com>
2026-04-01 20:23:19 +0300

    feat: add validation for request params

--- src/main/java/org/musicservice/demo/controller/rest/music/SearchMusicController.java
+++ src/main/java/org/musicservice/demo/controller/rest/music/SearchMusicController.java
@@ -1,5 +1,7 @@
 package org.musicservice.demo.controller.rest.music;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.musicservice.demo.dto.music.album.AlbumResponse;
 import org.musicservice.demo.dto.music.artist.ArtistResponse;
 import org.musicservice.demo.dto.music.common.PageResponse;
@@ -7,8 +9,10 @@ import org.musicservice.demo.dto.music.sound.SoundResponse;
 import org.musicservice.demo.service.search.SearchMusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequestMapping("/api/search")
 public class SearchMusicController {
@@ -23,24 +27,24 @@ public class SearchMusicController {
     @GetMapping("/{fragment}/artists")
     public ResponseEntity<PageResponse<ArtistResponse>> foundArtistsView(
             @PathVariable (value = "fragment") String fragment,
-            @RequestParam(name = "page") int page,
-            @RequestParam(name = "size") int size){
+            @RequestParam (name = "page") @Min(0) int page,
+            @RequestParam(name = "size") @Min(1) @Max(30) int size){
         return ResponseEntity.ok(searchMusicService.getArtistsByNameStartingWith(fragment, page, size));
     }
 
     @GetMapping("/{fragment}/albums")
     public ResponseEntity<PageResponse<AlbumResponse>> foundAlbumsView(
             @PathVariable (value = "fragment") String fragment,
-            @RequestParam(name = "page") int page,
-            @RequestParam(name = "size") int size){
+            @RequestParam (name = "page") @Min(0) int page,
+            @RequestParam(name = "size") @Min(1) @Max(30) int size){
         return ResponseEntity.ok(searchMusicService.getAlbumsByTitleStartingWith(fragment, page, size));
     }
 
     @GetMapping("/{fragment}/tracks")
     public ResponseEntity<PageResponse<SoundResponse>> foundTracksView(
             @PathVariable (value = "fragment") String fragment,
-            @RequestParam(name = "page") int page,
-            @RequestParam(name = "size") int size){
+            @RequestParam (name = "page") @Min(0) int page,
+            @RequestParam(name = "size") @Min(1) @Max(30) int size){
         return ResponseEntity.ok(searchMusicService.getTracksByTitleStartingWith(fragment, page, size));
     }
 }
--- src/main/java/org/musicservice/demo/exception/handler/ValidationExceptionHandler.java
+++ src/main/java/org/musicservice/demo/exception/handler/ValidationExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.musicservice.demo.exception.handler;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.musicservice.demo.error.ApiErrorResponse;
 import org.musicservice.demo.error.ErrorType;
 import org.musicservice.demo.exception.user.RegistrationException;
@@ -9,14 +11,14 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ValidationExceptionHandler {
@@ -24,13 +26,13 @@ public class ValidationExceptionHandler {
     // Ошибки валидации DTO
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleValidationException(MethodArgumentNotValidException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         Map<String, List<String>> errors = new HashMap<>();
         BindingResult bindingResult = e.getBindingResult();
         List<FieldError> errorList = bindingResult.getFieldErrors();
         for(FieldError error: errorList){
             errors.computeIfAbsent(error.getField(), key-> new ArrayList<>()).add(error.getDefaultMessage());
         }
-        HttpStatus status = HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body(
                 new ApiErrorResponse(ErrorType.VALIDATION_ERROR.name(), "Validation failed",
                         status.value(), System.currentTimeMillis(), errors));
@@ -65,5 +67,32 @@ public class ValidationExceptionHandler {
         );
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiErrorResponse> handleConstraintViolation(ConstraintViolationException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, List<String>> error = e.getConstraintViolations().stream()
+                .collect(Collectors.groupingBy(
+                        violation -> violation.getPropertyPath().toString(),
+                        Collectors.mapping(ConstraintViolation::getMessage, Collectors.toList())
+                ));
+        return ResponseEntity.status(status).body(new ApiErrorResponse(ErrorType.VALIDATION_ERROR.name(),
+                "Ошибка ограничения параметра", status.value(), System.currentTimeMillis(), error));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, List<String>> error = Map.of(e.getName(), List.of(e.getMessage()));
+        return ResponseEntity.status(status).body(new ApiErrorResponse(ErrorType.VALIDATION_ERROR.name(),
+                "Не верный тип параметра", status.value(), System.currentTimeMillis(), error));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiErrorResponse> handleMissingServletRequestParameter(MissingServletRequestParameterException e){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, List<String>> error = Map.of(e.getParameterName(), List.of(e.getMessage()));
+        return ResponseEntity.status(status).body(new ApiErrorResponse(ErrorType.VALIDATION_ERROR.name(),
+                "Отсутсвует параметр запроса", status.value(), System.currentTimeMillis(), error));
+    }
 
 }
