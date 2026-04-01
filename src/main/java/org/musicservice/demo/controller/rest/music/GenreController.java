2dd446cf1036c2fb44185bea3c3a1acc88974da9
Igor <igor.bocharov.77@gmail.com>
2026-04-02 01:25:48 +0300

    feat: add validation for genre controller

--- src/main/java/org/musicservice/demo/controller/rest/music/GenreController.java
+++ src/main/java/org/musicservice/demo/controller/rest/music/GenreController.java
@@ -1,5 +1,7 @@
 package org.musicservice.demo.controller.rest.music;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.musicservice.demo.dto.music.album.AlbumResponse;
 import org.musicservice.demo.dto.music.common.PageResponse;
 import org.musicservice.demo.dto.music.genre.GenreResponse;
@@ -10,8 +12,10 @@ import org.musicservice.demo.service.music.GenreService;
 import org.musicservice.demo.service.music.SoundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequestMapping("/api/genre")
 public class GenreController {
@@ -40,16 +44,16 @@ public class GenreController {
     @GetMapping("/{id}/tracks")
     public ResponseEntity<PageResponse<SoundResponse>> pagedArtistsByGenre(
             @PathVariable("id") Long genreId,
-            @RequestParam(name = "page") int page,
-            @RequestParam(name = "size") int size){
+            @RequestParam(name = "page") @Min(0) int page,
+            @RequestParam(name = "size") @Min(1) @Max(30) int size){
         return ResponseEntity.ok(soundService.findTracksByGenreIdPaged(genreId, page, size));
     }
 
     @GetMapping("/{id}/albums")
     public ResponseEntity<PageResponse<AlbumResponse>> pagedAlbumsByGenre(
             @PathVariable("id") Long genreId,
-            @RequestParam(name = "page") int page,
-            @RequestParam(name = "size") int size){
+            @RequestParam(name = "page") @Min(0) int page,
+            @RequestParam(name = "size") @Min(1) @Max(30) int size){
         return ResponseEntity.ok(albumService.findAlbumsByGenreIdPaged(genreId, page, size));
     }
 
