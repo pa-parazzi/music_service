286e520ea3c76a2f98577375c614bfcd4fdef9bb
Igor <igor.bocharov.77@gmail.com>
2026-04-05 15:56:55 +0300

    refactor: drop unnecessary mapper class, replace to general image mapper

--- src/main/java/org/musicservice/demo/mapper/image/ImageMapper.java
+++ src/main/java/org/musicservice/demo/mapper/image/ImageMapper.java
@@ -3,11 +3,11 @@ package org.musicservice.demo.mapper.image;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.musicservice.demo.dto.image.ImageResponse;
-import org.musicservice.demo.entity.image.AlbumImage;
+import org.musicservice.demo.entity.image.UserAvatar;
 
 @Mapper(componentModel = "spring", uses = {ImageUrlMapper.class})
-public interface AlbumImageMapper {
+public interface ImageMapper {
 
     @Mapping(target = "url", source = "key", qualifiedByName = "mapImgUrl")
-    ImageResponse convertToDto(AlbumImage albumImage);
+    ImageResponse toImageResponse(UserAvatar userAvatar);
 }
