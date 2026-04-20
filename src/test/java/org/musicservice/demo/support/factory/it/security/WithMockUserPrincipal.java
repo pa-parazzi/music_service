159f9efc8419c527dcf3815d598d41144526b89d
Igor <igor.bocharov.77@gmail.com>
2026-04-20 23:06:22 +0300

    refactor: add authority parameter

--- src/test/java/org/musicservice/demo/support/factory/it/security/WithMockUserPrincipal.java
+++ src/test/java/org/musicservice/demo/support/factory/it/security/WithMockUserPrincipal.java
@@ -1,5 +1,6 @@
 package org.musicservice.demo.support.factory.it.security;
 
+import org.musicservice.demo.entity.user.Authority;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 import java.lang.annotation.ElementType;
@@ -12,4 +13,5 @@ import java.lang.annotation.Target;
 @WithSecurityContext(factory = WithMockUserPrincipalSecurityContextFactory.class)
 public @interface WithMockUserPrincipal {
     long userId() default 1L;
+    Authority authority() default Authority.USER;
 }
--- src/test/java/org/musicservice/demo/support/factory/it/security/WithMockUserPrincipalSecurityContextFactory.java
+++ src/test/java/org/musicservice/demo/support/factory/it/security/WithMockUserPrincipalSecurityContextFactory.java
@@ -1,23 +1,18 @@
 package org.musicservice.demo.support.factory.it.security;
 
-import org.musicservice.demo.entity.user.Authority;
-import org.musicservice.demo.security.userDetails.UserPrincipal;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WithMockUserPrincipalSecurityContextFactory implements WithSecurityContextFactory<WithMockUserPrincipal> {
 
     @Override
     public SecurityContext createSecurityContext(WithMockUserPrincipal annotation) {
-        Authentication authentication = new UsernamePasswordAuthenticationToken(annotation.userId(), null, List.of(Authority.USER));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(annotation.userId(), null, List.of(annotation.authority()));
         SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
         securityContext.setAuthentication(authentication);
         return securityContext;
