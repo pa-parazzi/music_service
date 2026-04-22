f96bf63eaba86ace5294a98366c299b1d442f6b2
Igor <igor.bocharov.77@gmail.com>
2026-04-22 10:22:10 +0300

    fix N + 1 ussie in SoundLikeRepository

--- src/main/java/org/musicservice/demo/repository/likes/SoundLikeRepository.java
+++ src/main/java/org/musicservice/demo/repository/likes/SoundLikeRepository.java
@@ -3,6 +3,7 @@ package org.musicservice.demo.repository.likes;
 import org.musicservice.demo.entity.likes.SoundLike;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,7 @@ public interface SoundLikeRepository extends JpaRepository<SoundLike, Long> {
 
     void deleteByUserIdAndSoundId(Long userId, Long soundId);
 
+    @EntityGraph(attributePaths = "sound")
     Page<SoundLike> findByUserIdOrderByCreatedAtDescIdDesc(Long userId, Pageable pageable);
 
     boolean existsByUserIdAndSoundId(Long userId, Long soundId);
