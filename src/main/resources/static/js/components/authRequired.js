1d3f5e3039bd3661643bfe475f0466bd79b93e46
Igor <igor.bocharov.77@gmail.com>
2026-05-08 02:47:54 +0300

    feat: add render auth required message

---
+++ src/main/resources/static/js/components/authRequired.js
@@ -0,0 +1,5 @@
+export function renderAuthRequired(container){
+    container.innerHTML = `
+         <h2 class="page-state">
+         Войдите или зарегистрируйтесь, чтобы добавлять коллекции</h2>`
+}
