593e1b9eec42287b093e087713dd3cb226d28793
Igor <igor.bocharov.77@gmail.com>
2026-05-08 03:12:02 +0300

    refactor (spa): split search page into overview and type-specific content pages

---
+++ src/main/resources/static/js/pages/searchContent.js
@@ -0,0 +1,146 @@
+import {
+    getFoundAlbumsByFragmentPaged,
+    getFoundArtistsByFragmentPaged,
+    getFoundSoundsByFragmentPaged
+} from "../api/searchApi.js";
+import {getLikedSoundsIds} from "../api/soundLikesApi.js";
+import {getToken} from "../user/refreshAccessToken.js";
+import {initSoundsDelegation, loadSoundsPaged} from "../module/sounds.js";
+import {initPlayAlbumCardsDelegation, loadAlbumsPaged} from "../module/albums.js";
+import {paginationStateOfAlbums, paginationStateOfArtists, paginationStateOfSounds} from "../store/paginationState.js";
+import {renderArtistsLayout} from "../components/artistsView.js";
+import {renderAlbumsLayout} from "../components/albumsView.js";
+import {renderSoundsLayout} from "../components/soundsView.js";
+import {initInfiniteScroll, resetPaginationState} from "../utils/util.js";
+import {loadArtistsPaged} from "../module/artists.js";
+import {loadCss, unloadCss} from "../core/resources.js";
+
+export async function initSearchContentPage({fragment, type}) {
+    document.title = "Поиск";
+
+    const jwt = getToken();
+
+    resetPaginationState();
+
+    const appContainer = document.getElementById("app");
+
+    const handlers = {
+        artists: () => initArtistsByFragmentPage(),
+        albums: () => initAlbumsByFragmentPage(),
+        tracks: () => initSoundsByFragmentPage()
+    };
+
+    const handler = handlers[type];
+    const cleanUpFn = await handler();
+
+    return function cleanupPage() {
+        cleanUpFn?.();
+    };
+
+    async function initArtistsByFragmentPage(){
+        const artistCardCss = loadCss("/css/components/artist-card.css");
+
+        paginationStateOfArtists.size = 10;
+
+        renderArtistsLayout(appContainer);
+
+        const artistsHeading = appContainer.querySelector(".artists-heading");
+        const artistsContainer = appContainer.querySelector(".artist-rows");
+        const scrollAnchor = appContainer.querySelector(".scroll-anchor");
+
+        artistsHeading.textContent = "Исполнители по запросу " + "\"" + fragment + "\"";
+
+        const pageResponseOfArtists = await getFoundArtistsByFragmentPaged(fragment);
+        loadArtistsPaged(pageResponseOfArtists, artistsContainer);
+
+        const infiniteScroll = initInfiniteScroll({
+            loadFn: async () => {
+                const pageResponseOfArtists = await getFoundArtistsByFragmentPaged(fragment);
+                loadArtistsPaged(pageResponseOfArtists, artistsContainer);
+            },
+            hasNextFn: () => paginationStateOfArtists.hasNext,
+            isLoadingFn: () => paginationStateOfArtists.isLoading,
+            anchor: scrollAnchor
+        });
+        await infiniteScroll.init();
+
+        return function cleanUp(){
+            unloadCss(artistCardCss);
+            appContainer.innerHTML = "";
+        }
+    }
+
+    async function initAlbumsByFragmentPage(){
+        const albumsCardCss = loadCss("/css/components/albums-card-rows.css");
+
+        paginationStateOfAlbums.size = 14;
+
+        renderAlbumsLayout(appContainer);
+
+        const albumsHeading = appContainer.querySelector(".album-rows-heading");
+        const albumsContainer = appContainer.querySelector(".album-rows");
+        const scrollAnchor = appContainer.querySelector(".scroll-anchor");
+
+        albumsHeading.textContent = "Альбомы по запросу " + "\"" + fragment + "\"";
+
+        const pageResponseOfAlbums = await getFoundAlbumsByFragmentPaged(fragment);
+        loadAlbumsPaged(pageResponseOfAlbums, albumsContainer);
+
+        const removePlayAlbumsDelegation = initPlayAlbumCardsDelegation(albumsContainer);
+
+        const infiniteScroll = initInfiniteScroll({
+            loadFn: async () => {
+                const pageResponseOfAlbums = await getFoundAlbumsByFragmentPaged(fragment);
+                loadAlbumsPaged(pageResponseOfAlbums, albumsContainer);
+            },
+            hasNextFn: () => paginationStateOfAlbums.hasNext,
+            isLoadingFn: () => paginationStateOfAlbums.isLoading,
+            anchor: scrollAnchor
+        });
+        await infiniteScroll.init();
+
+        return function cleanUp(){
+            removePlayAlbumsDelegation?.();
+            unloadCss(albumsCardCss);
+            appContainer.innerHTML = "";
+        }
+    }
+
+    async function initSoundsByFragmentPage(){
+        const soundsCss = loadCss("/css/components/sounds.css");
+
+        paginationStateOfSounds.size = 20;
+
+        renderSoundsLayout(appContainer);
+
+        const soundsHeading = appContainer.querySelector(".sounds-heading");
+        const soundsContainer = appContainer.querySelector(".sounds");
+        const scrollAnchor = appContainer.querySelector(".scroll-anchor");
+
+        soundsHeading.textContent = "Треки по запросу " + "\"" + fragment + "\"";
+
+        const likedSoundsResponse = await getLikedSoundsIds(jwt);
+        const likedSoundsIds = new Set(likedSoundsResponse.ids);
+
+        const pageResponseOfSounds = await getFoundSoundsByFragmentPaged(fragment);
+        loadSoundsPaged(pageResponseOfSounds, soundsContainer, likedSoundsIds);
+        const removeSoundsDelegation = initSoundsDelegation(soundsContainer, likedSoundsIds, jwt);
+
+        const infiniteScroll = initInfiniteScroll({
+            loadFn: async () => {
+                const pageResponseOfSounds = await getFoundSoundsByFragmentPaged(fragment);
+                loadSoundsPaged(pageResponseOfSounds, soundsContainer, likedSoundsIds);
+            },
+            hasNextFn: () => paginationStateOfSounds.hasNext,
+            isLoadingFn: () => paginationStateOfSounds.isLoading,
+            anchor: scrollAnchor
+        });
+        await infiniteScroll.init();
+
+        return function cleanUp(){
+            removeSoundsDelegation?.();
+            unloadCss(soundsCss);
+            appContainer.innerHTML = "";
+        }
+    }
+}
---
+++ src/main/resources/static/js/pages/searchOverview.js
@@ -0,0 +1,77 @@
+import {renderSearchOverviewLayout} from "../components/searchView.js";
+import {
+    getFoundAlbumsByFragmentPaged,
+    getFoundArtistsByFragmentPaged,
+    getFoundSoundsByFragmentPaged
+} from "../api/searchApi.js";
+import {renderArtists} from "../components/artistsView.js";
+import {renderAlbumCards} from "../components/albumsView.js";
+import {initPlayAlbumCardsDelegation} from "../module/albums.js";
+import {getLikedSoundsIds} from "../api/soundLikesApi.js";
+import {renderSounds} from "../components/soundsView.js";
+import {paginationStateOfSounds} from "../store/paginationState.js";
+import {initSoundsDelegation} from "../module/sounds.js";
+import {getToken} from "../user/refreshAccessToken.js";
+import {loadCss, unloadCss} from "../core/resources.js";
+import {resetPaginationState} from "../utils/util.js";
+
+export async function initSearchOverviewPage({fragment}){
+    const contentSectionsCss = loadCss("/css/layout/content-sections.css");
+    const albumsCardCss = loadCss("/css/components/albums-card-rows.css");
+    const artistCardCss = loadCss("/css/components/artist-card.css");
+    const soundsCss = loadCss("/css/components/sounds.css");
+
+    document.title = "Поиск";
+
+    const jwt = getToken();
+
+    resetPaginationState();
+
+    const appContainer = document.getElementById("app");
+
+    renderSearchOverviewLayout(appContainer, fragment);
+
+    const artistsContainer = appContainer.querySelector(".artists");
+    const albumsContainer = appContainer.querySelector(".albums");
+    const soundsContainer = appContainer.querySelector(".sounds");
+
+    const notFoundContainer = appContainer.querySelector(".not-found");
+
+    const artistsPageResponse = await getFoundArtistsByFragmentPaged(fragment);
+    const artists = artistsPageResponse.content;
+
+    const albumsPageResponse = await getFoundAlbumsByFragmentPaged(fragment);
+    const albums = albumsPageResponse.content;
+
+    const soundsPageResponse = await getFoundSoundsByFragmentPaged(fragment);
+    const sounds = soundsPageResponse.content;
+
+    if((artistsPageResponse.status === 204) &&
+        (albumsPageResponse.status === 204) &&
+        (soundsPageResponse.status === 204)) {
+        notFoundContainer.textContent = "По запросу " + "\"" + fragment + "\""+ " ничего не найдено";
+        return;
+    }
+
+    renderArtists(artistsContainer, artists);
+
+    renderAlbumCards(albumsContainer, albums);
+    const removePlayAlbumsDelegation = initPlayAlbumCardsDelegation(albumsContainer);
+
+    const likedSoundsResponse = await getLikedSoundsIds(jwt);
+    const likedSoundsIds = new Set(likedSoundsResponse.ids);
+
+    renderSounds({container: soundsContainer, soundList: sounds, likedSoundsIds: likedSoundsIds});
+    paginationStateOfSounds.sounds = sounds;
+    const removeSoundsDelegation = initSoundsDelegation(soundsContainer, likedSoundsIds, jwt);
+
+    return function cleanUp(){
+        removePlayAlbumsDelegation?.();
+        removeSoundsDelegation?.();
+        unloadCss(contentSectionsCss);
+        unloadCss(albumsCardCss);
+        unloadCss(artistCardCss);
+        unloadCss(soundsCss);
+        appContainer.innerHTML = "";
+    }
+}
