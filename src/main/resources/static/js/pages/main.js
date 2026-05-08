ed31f6b8cddfd2561483d85d870e69f99ec93bbe
Igor <igor.bocharov.77@gmail.com>
2026-05-08 03:18:38 +0300

    refactor (spa): adapt main page lifecycle to SPA architecture

--- src/main/resources/static/js/pages/main.js
+++ src/main/resources/static/js/pages/main.js
@@ -1,33 +1,40 @@
-import {initSearchForm} from "../module/search.js";
-import {initSidebar} from "../module/sidebar.js";
-import {initPlayer} from "../module/player.js";
 import {getNewAlbumReleasesPaged} from "../api/albumApi.js";
 import {initPlayAlbumCardsDelegation, loadAlbumsPaged} from "../module/albums.js";
 import {getGenres} from "../api/genreApi.js";
 import {resetPaginationState} from "../utils/util.js";
 import {paginationStateOfAlbums} from "../store/paginationState.js";
 import {renderGenresWithLimit} from "../components/genresView.js";
+import {renderMainPageLayout} from "../components/mainView.js";
+import {loadCss, unloadCss} from "../core/resources.js";
 
-async function initMainPage(){
-    initPlayer();
-    const searchForm = document.getElementById("search-form");
-    initSearchForm(searchForm);
+export async function initMainPage(){
+    const albumsCardCss = loadCss("/css/components/albums-card-rows.css");
+    const genresCardCss = loadCss("/css/components/genres-card-rows.css");
+    const contentSectionsCss = loadCss("/css/layout/content-sections.css");
 
-    const albumsContainer = document.querySelector(".album-rows");
-    const genresContainer = document.querySelector(".genre-rows");
+    document.title = "Главная";
+
+    const appContainer = document.getElementById("app");
+    renderMainPageLayout(appContainer);
+
+    const albumsContainer = appContainer.querySelector(".album-rows");
+    const genresContainer = appContainer.querySelector(".genre-rows");
 
     resetPaginationState();
     paginationStateOfAlbums.size = 7;
     const pageResponseOfNewAlbumReleases = await getNewAlbumReleasesPaged();
     loadAlbumsPaged(pageResponseOfNewAlbumReleases, albumsContainer);
-    initPlayAlbumCardsDelegation(albumsContainer);
+    const removePlayAlbumsDelegation = initPlayAlbumCardsDelegation(albumsContainer);
 
     const genresResponse = await getGenres();
     const genres = genresResponse.genres;
     renderGenresWithLimit(genresContainer, genres, 7);
-}
 
-document.addEventListener("componentsLoaded", async ()=> {
-    initSidebar();
-    await initMainPage();
-});
+    return function cleanUp(){
+        removePlayAlbumsDelegation?.();
+        unloadCss(contentSectionsCss);
+        unloadCss(albumsCardCss);
+        unloadCss(genresCardCss);
+        appContainer.innerHTML = "";
+    }
+}
