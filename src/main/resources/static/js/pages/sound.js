6abc54d66e166758bde9be889ee56d5f250820a7
Igor <igor.bocharov.77@gmail.com>
2026-05-08 03:18:45 +0300

    refactor (spa): adapt sound page lifecycle to SPA architecture

--- src/main/resources/static/js/pages/sound.js
+++ src/main/resources/static/js/pages/sound.js
@@ -1,42 +1,44 @@
 import {formatTime} from "../utils/util.js";
 import {getToken} from "../user/refreshAccessToken.js";
-import {initSidebar} from "../module/sidebar.js";
 import {getSoundById} from "../api/soundApi.js";
 import {getSoundLikeStatusBySoundId} from "../api/soundLikesApi.js";
 import {initSoundLikeBySoundId} from "../module/soundLikes.js";
-import {initSearchForm} from "../module/search.js";
-import {initPlayer} from "../module/player.js";
 import {initPlaySoundButton} from "../module/sounds.js";
 import {playerState} from "../store/playerState.js";
 import {renderSoundPage} from "../components/soundsView.js";
+import {loadCss, unloadCss} from "../core/resources.js";
 
-async function initSoundPage(){
-    initPlayer();
-    const searchForm = document.getElementById("search-form");
-    initSearchForm(searchForm);
+export async function initSoundPage({id}){
+    const soundPageCss = loadCss("/css/pages/sound.css");
+    const albumsCardCss = loadCss("/css/components/albums-card-rows.css");
 
     const jwt = getToken();
-    const soundId = window.location.pathname.split('/').pop();
 
-    const mainContainer = document.getElementById("main-container");
+    const soundId = Number(id);
+
+    const appContainer = document.getElementById("app");
 
     const sound = await getSoundById(soundId);
     const artist = sound.artist;
     const album = sound.album;
     const trackDuration = formatTime(sound.duration);
 
-    renderSoundPage(mainContainer, sound, trackDuration, artist, album);
+    const soundPageContainer = renderSoundPage(appContainer, sound, trackDuration, artist, album);
 
-    const playSoundBtn = mainContainer.querySelector(".play-sound-btn");
+    const playSoundBtn = soundPageContainer.querySelector(".play-sound-btn");
+    const likeBtn = soundPageContainer.querySelector(".like-btn");
+
     playerState.currentPlaySoundButton = playSoundBtn;
-    initPlaySoundButton(soundId, sound, playSoundBtn);
+    const removePlaySoundsDelegation = initPlaySoundButton(soundId, sound, playSoundBtn);
 
-    const likeBtn = mainContainer.querySelector(".like-btn");
     const likeSoundStatus = await getSoundLikeStatusBySoundId(jwt, soundId);
-    await initSoundLikeBySoundId(jwt, likeSoundStatus, likeBtn, soundId);
-}
+    const removeSoundLikeDelegation = initSoundLikeBySoundId(jwt, likeSoundStatus, likeBtn, soundId);
 
-document.addEventListener("componentsLoaded", async () => {
-    initSidebar();
-    await initSoundPage();
-});
+    return function cleanUp(){
+        removePlaySoundsDelegation?.();
+        removeSoundLikeDelegation?.();
+        unloadCss(soundPageCss);
+        unloadCss(albumsCardCss);
+        appContainer.innerHTML = "";
+    }
+}
