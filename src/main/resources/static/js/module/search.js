100d0fee61b429f4169ee424ba5d58a13dc5bb9f
Igor <igor.bocharov.77@gmail.com>
2026-05-08 03:01:41 +0300

    refactor (spa): remove page reload when submitting search form

--- src/main/resources/static/js/module/search.js
+++ src/main/resources/static/js/module/search.js
@@ -1,10 +1,11 @@
 export function initSearchForm(searchForm){
+    const input = searchForm.querySelector(".search-input");
+
     searchForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        const fragment = document.getElementById("search-input").value.trim();
-        if (!fragment) {
-            return;
-        }
-        window.location.href = `/search/${encodeURIComponent(fragment)}`;
+        const fragment = input.value.trim();
+        if (!fragment) return;
+        history.pushState({}, "", `/search/${encodeURIComponent(fragment)}`);
+        window.dispatchEvent(new PopStateEvent("popstate"));
     });
 }
