857ca15efdf4494e56a0b74fe3a9e1d0e1f6b583
Igor <igor.bocharov.77@gmail.com>
2026-05-10 12:20:19 +0300

    refactor: align classes name for css style

--- src/main/resources/static/js/components/loginForm.js
+++ src/main/resources/static/js/components/loginForm.js
@@ -2,18 +2,18 @@ export function loadLoginForm(container){
     container.innerHTML = `
     <div class="form-wrapper">
       <form class="login-form">
-        <label for="username">Имя пользователя: </label>
+        <label class="field-name" for="username">Имя пользователя: </label>
         <input type="text" name="username" class="username">
         <div class="username-error"></div>
 
-        <label for="password">Пароль: </label>
+        <label class="field-name" for="password">Пароль: </label>
         <input type="password" name="password" class="password">
         <div class="password-error"></div>
 
         <div class="message error-message"></div>
         <div class="message success-message"></div>
 
-        <button type="submit">Войти</button>
+        <button class="submit-login-button" type="submit">Войти</button>
       </form>
     </div>`;
 }
--- src/main/resources/static/js/components/registrationForm.js
+++ src/main/resources/static/js/components/registrationForm.js
@@ -2,27 +2,27 @@ export function loadRegistrationForm(container){
     container.innerHTML = `
   <div class="form-wrapper">
     <form class="registration-form" enctype="multipart/form-data">
-        <label for="username">Имя пользователя: </label>
+        <label class="field-name" for="username">Имя пользователя: </label>
         <input type="text" name="username" class="username" required>
         <div class="username-error"></div>
         <br/>
 
-        <label for="password">Пароль: </label>
+        <label class="field-name" for="password">Пароль: </label>
         <input type="password" name="password" class="password" required>
         <div class="password-error"></div>
         <br/>
 
-        <label for="email">Почта: </label>
+        <label class="field-name" for="email">Почта: </label>
         <input type="email" name="email" class="email" required>
         <div class="email-error"></div>
         <br/>
 
-        <label for="date-of-birth">Дата рождения: </label>
+        <label class="field-name" for="date-of-birth">Дата рождения: </label>
         <input type="text" name="date-of-birth" class="date-of-birth" required>
         <div class="date-of-birth-error"></div>
         <br/>
 
-        <label for="avatar">Аватар: </label>
+        <label class="field-name" for="avatar">Аватар: </label>
         <input type="file" name="avatar" class="avatar" accept="image/*">
         <div class="avatar-error"></div>
 
@@ -30,7 +30,7 @@ export function loadRegistrationForm(container){
         <div class="message error-message"></div>
         <br/>
 
-        <button type="submit">Зарегистрироваться</button>
+        <button class="submit-registration-button" type="submit">Зарегистрироваться</button>
     </form>
   </div>`;
 }
